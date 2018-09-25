cd6aab87f33cb968231b6de471b2f21e0f8bd065
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2018-09-25 20:17:26 +0900

    Enable to write x and o

--- react-tic-tac-toe/src/index.js
+++ react-tic-tac-toe/src/index.js
@@ -3,32 +3,58 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         value: null,
+    //     };
+    // }
 
     render() {
         return (
             <button
                 className="square"
-                onClick={() => { this.setState({ value: 'x' }); }}
+                onClick={this.props.onSquareClick}
             >
-                {this.state.value}
+                {this.props.value}
             </button>
         );
     }
 }
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true,
+        }
+    }
+
     renderSquare(i) {
-        return <Square value={i} />;
+        return (
+        <Square
+          value={this.state.squares[i]}
+          onSquareClick={() => this.handleClick(i)}
+         />
+        );
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = this.nextMark();
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
+    }
+
+    nextMark () {
+        return this.state.xIsNext ? 'x' : 'o';
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: ' + this.nextMark();
 
         return (
             <div>
