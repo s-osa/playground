c6e03c8ad9213f07b37026cbcf5f57bc76f1d868
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2018-09-27 19:50:08 +0900

    first step

---
+++ react-spa-sample2/webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    mode: 'development',
+    entry: './index.js', // エントリポイントのjsxファイル
+    output: {
+        filename: 'bundle.js' // 出力するファイル
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/, // 拡張子がjsで
+                exclude: /node_modules/, // node_modulesフォルダ配下は除外
+                use: {
+                    loader: 'babel-loader', // babel-loaderを使って変換する
+                    options: {
+                        plugins: ["transform-react-jsx"] // babelのtransform-react-jsxプラグインを使ってjsxを変換
+                    }
+                }
+            }
+        ]
+    }
+}
