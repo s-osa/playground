493ecdab713652d34a7d4ee7e7f99c7f4a90f9f3
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2020-09-21 11:52:52 +0900

    Init new Xcode project

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by shunsuke-osa on 2020/09/21.
+//  Copyright © 2020 shunsuke-osa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
