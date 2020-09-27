f5bc78c82022fb759ed0f8c9785be69a2ed684fd
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2020-09-28 02:22:03 +0900

    Create a Table View

---
+++ FoodTracker/FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by shunsuke-osa on 2020/09/28.
+//  Copyright © 2020 shunsuke-osa. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    //MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
